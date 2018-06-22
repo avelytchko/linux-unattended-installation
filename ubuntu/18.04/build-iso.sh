3f0a482d11e7f55a51d57742684ab0826ec0439c
Alex Velychko <avelytchko@gmail.com>
2018-06-22 19:03:07 -0400

    Change script with hardcoded values

--- ubuntu/18.04/build-iso.sh
+++ ubuntu/18.04/build-iso.sh
@@ -6,15 +6,7 @@ set -e
 : "${BIN_XORRISO:=$(type -P xorriso)}"
 
 # get parameters
-SSH_PUBLIC_KEY_FILE=${1:-"$HOME/.ssh/id_rsa.pub"}
-TARGET_ISO=${2:-"`pwd`/ubuntu-18.04-netboot-amd64-unattended.iso"}
-
-# check if ssh key exists
-if [ ! -f "$SSH_PUBLIC_KEY_FILE" ];
-then
-    echo "Error: public SSH key $SSH_PUBLIC_KEY_FILE not found!"
-    exit 1
-fi
+TARGET_ISO=${1:-"`pwd`/ubuntu-18.04-netboot-amd64-unattended.iso"}
 
 # get directories
 CURRENT_DIR="`pwd`"
@@ -25,6 +17,7 @@ TMP_INITRD_DIR="`mktemp -d`"
 
 # download and extract netboot iso
 SOURCE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
+#SOURCE_ISO_URL="http://cdimage.ubuntu.com/releases/18.04/release/ubuntu-18.04-server-amd64.iso"
 cd "$TMP_DOWNLOAD_DIR"
 wget -4 "$SOURCE_ISO_URL" -O "./netboot.iso"
 "$BIN_7Z" x "./netboot.iso" "-o$TMP_DISC_DIR"
@@ -38,8 +31,6 @@ patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
 cd "$TMP_INITRD_DIR"
 mkdir "./custom"
 cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
-cp "$SSH_PUBLIC_KEY_FILE" "./custom/userkey.pub"
-cp "$SCRIPT_DIR/custom/ssh-host-keygen.service" "./custom/ssh-host-keygen.service"
 
 # append assets to initrd image
 cd "$TMP_INITRD_DIR"
@@ -61,5 +52,3 @@ rm -r "$TMP_DOWNLOAD_DIR"
 rm -r "$TMP_DISC_DIR"
 rm -r "$TMP_INITRD_DIR"
 
-# done
-echo "Next steps: install system, login via root, adjust the authorized keys, set a root password (if you want to), deploy via ansible (if applicable), enjoy!"
