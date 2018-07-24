4ecf8e21732eecd2238c3c41ea0da39a7ecd0923
Alex Velychko <avelytchko@gmail.com>
2018-07-25 01:15:19 +0300

    Fix debian image creation

--- debian/9/build-iso.sh
+++ debian/9/build-iso.sh
@@ -16,14 +16,14 @@ TMP_DISC_DIR="`mktemp -d`"
 TMP_INITRD_DIR="`mktemp -d`"
 
 # download and extract netboot iso
-SOURCE_ISO_URL="https://cdimage.debian.org/debian-cd/9.5.0/amd64/iso-cd/debian-9.5.0-amd64-netinst.iso"
+SOURCE_ISO_URL="http://ftp.ua.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/mini.iso"
 cd "$TMP_DOWNLOAD_DIR"
 wget -4 "$SOURCE_ISO_URL" -O "./netboot.iso"
 "$BIN_7Z" x "./netboot.iso" "-o$TMP_DISC_DIR"
 
 # patch boot menu
 cd "$TMP_DISC_DIR"
-dos2unix "./isolinux/isolinux.cfg"
+dos2unix "./isolinux.cfg"
 patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
 
 # prepare assets
@@ -33,16 +33,15 @@ cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
 
 # append assets to initrd image
 cd "$TMP_INITRD_DIR"
-cat "$TMP_DISC_DIR/install.amd/initrd.gz" | gzip -d > "./initrd"
+cat "$TMP_DISC_DIR/initrd.gz" | gzip -d > "./initrd"
 echo "./preseed.cfg" | fakeroot cpio -o -H newc -A -F "./initrd"
 find "./custom" | fakeroot cpio -o -H newc -A -F "./initrd"
-cat "./initrd" | gzip -9c > "$TMP_DISC_DIR/install.amd/initrd.gz"
+cat "./initrd" | gzip -9c > "$TMP_DISC_DIR/initrd.gz"
 
 # build iso
 cd "$TMP_DISC_DIR"
 rm -r '[BOOT]'
-"$BIN_XORRISO" -as mkisofs -r -V "debian_9_netboot_unattended" -J -b isolinux/isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -o "$TARGET_ISO" ./
-#xorriso -as mkisofs -o preseed-debian-9.3.0-i386-netinst.iso -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table isofiles
+"$BIN_XORRISO" -as mkisofs -r -V "debian_9_netboot_unattended" -J -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -o "$TARGET_ISO" ./
 
 # go back to initial directory
 cd "$CURRENT_DIR"
